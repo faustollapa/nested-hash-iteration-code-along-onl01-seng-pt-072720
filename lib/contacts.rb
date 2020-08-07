05dd67a15dacf362c9b10a24de3a4e2214190960
Fausto Llapa <faustollapa850@gmail.com>
2020-08-07 14:21:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,4 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  
+   freddy = contacts["Freddy Mercury"]
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
+end 
