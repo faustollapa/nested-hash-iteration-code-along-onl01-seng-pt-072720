571a5d58fe21d055703dddba2438ef2c30592afd
Fausto Llapa <faustollapa850@gmail.com>
2020-08-07 14:20:26 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,16 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  puts remove_strawberry(contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_icecream_flavors: ["chocolate", "vanilla"]
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
-  })
-end
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_icecream_flavors
+        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
+end  
